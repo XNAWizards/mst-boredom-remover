5b1ef65cf97589d162917af6693aac044170e225
James Trey Nickelsen <tarnasa7@gmail.com>
2015-10-14 12:28:10 -0500

    Normalize variables for Tile

--- mst-boredom-remover/mst-boredom-remover/engine/Tile.cs
+++ mst-boredom-remover/mst-boredom-remover/engine/Tile.cs
@@ -9,7 +9,7 @@ namespace mst_boredom_remover
     {
         public int id;
         public Position position;
-        public TileType tile_type;
+        public TileType tileType;
 
         public enum TileModifier
         {
@@ -17,7 +17,7 @@ namespace mst_boredom_remover
             Freezing,
             Windy
         };
-        public List<TileModifier> tile_modifiers;
+        public List<TileModifier> tileModifiers;
 
         public List<Tile> neighbors;
 
